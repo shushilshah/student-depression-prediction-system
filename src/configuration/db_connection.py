3431d7ee8b6151de4bfd3e3e5341691a34a489b5
ShushilShah <shahshushil3@gmail.com>
2025-03-22 07:57:12 +0545

    updated code with env file

--- src/configuration/db_connection.py
+++ src/configuration/db_connection.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+mongo_db = os.getenv("MONGO_DB")
+mongo_collection = os.getenv("MONGO_COLLECTION")
 
 df = pd.read_csv("data\student_depression_dataset.csv")
 
 # connect to MongoDB
 
-client = MongoClient(
-    "mongodb+srv://shushil:admin123@cluster0.y6vd8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client["student_depression_prediction_db"]
-collection = db['depression_data']
+client = MongoClient(mongo_uri)
+db = client[mongo_db]
+collection = db[mongo_collection]
 
 # Inserting data
 collection.delete_many({})
